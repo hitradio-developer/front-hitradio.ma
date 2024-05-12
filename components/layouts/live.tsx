b086b06f54a5acef563eeac6e97f5245f85ecc76
githitradio-developer <hitradiodeveloper@gmail.com>
2024-05-12 23:34:45 +0100

    create live bare

--- components/layouts/live.tsx
+++ components/layouts/live.tsx
@@ -1,19 +1,33 @@
 import Image from 'next/image'
 import Player from '../../public/logo/Play.png'
-import Hitradio from '../../public/podcast/podcats2.jpeg'
+import Hitradio from '../../public/podcast/podcats1.jpeg'
+import Link from 'next/link';
 
 export default function Live() {
     return (
       <>
-<div className="lower-fixed-section">
-    <div className='live-hit'>
-    <Image src={Hitradio} alt={''} 
-                className=''></Image>
-    </div>
-          <div className="player-audio">
-            <Image src={Player} alt={''} 
-                className=''></Image>
-          </div>
+      <div>
+        <div className="lower-fixed-section">
+            <div className='live-hit'>
+            <Link href='#'>
+                <Image src={Hitradio} alt={''} 
+                    className='img-live'>
+                </Image>
+            </Link>
+            <ul>
+                <li>Adrenalina</li>
+                <li>BABY GANG / BLANCO / MARRACASH</li>
+            </ul>
+                
+            </div>
+            <div className="player-audio">
+            <Link href='#'>
+
+                <Image src={Player} alt={''} 
+                className='w-[40px]'></Image>
+            </Link>
+            </div>
+        </div>
       </div>
       </>
   );}
